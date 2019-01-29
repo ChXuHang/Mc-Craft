6f6888947e40c0178b4b9252d0bb6c756a1bc032
ChXuHang <47051723+ChXuHang@users.noreply.github.com>
2019-01-29 19:46:35 +0800

    Object Upload
    tips:updata0001;

---
+++ js/craft.js
@@ -0,0 +1,187 @@
+function scr(name){
+  console.log(name);
+    switch (name) {
+      case "木板":
+        i.src="imgs/wooden_plank.gif";
+        break;
+      case "钟":
+        i.src="imgs/clock.png";
+        break;
+      case "指南针":
+        i.src="imgs/compass.png";
+        break;
+      case "地图":
+        i.src="imgs/map.png";
+        break;
+      case "书架":
+        i.src="imgs/bookshelf.png";
+        break;
+      case "地图":
+        i.src="imgs/map.png";
+        break;
+      case "音符盒":
+        i.src="imgs/noteblock.png";
+        break;
+      case "画":
+        i.src="imgs/painting.png";
+        break;
+      case "箭":
+        i.src="imgs/arrow.png";
+        break;
+      case "花岗岩":
+        i.src="imgs/granite.png";
+        break;
+      case "闪长岩":
+        i.src="imgs/diorite.png";
+        break;
+      case "暗海晶石":
+        i.src="imgs/darkprismarine.png";
+        break;
+      case "海晶灯":
+        i.src="imgs/sealantern.png";
+        break;
+      case "砂土":
+        i.src="imgs/coarsedirt.png";
+        break;
+      case "苔石":
+        i.src="imgs/mossstone.png";
+        break;
+      case "錾制石砖":
+        i.src="imgs/chiseledstonebrick.png";
+        break;
+      case "活塞":
+        i.src="imgs/piston.png";
+        break;
+      case "栅栏门":
+        i.src="imgs/fencegate.gif";
+        break;
+      case "中继器":
+        i.src="imgs/repeater.png";
+        break;
+      case "比较器":
+        i.src="imgs/comparator.png";
+        break;
+      case "铁轨":
+        i.src="imgs/rail.png";
+        break;
+      case "充能铁轨":
+        i.src="imgs/poweredrail.png";
+        break;
+      case "探测铁轨":
+        i.src="imgs/detectorrail.png";
+        break;
+      case "红石灯":
+        i.src="imgs/lamp.png";
+        break;
+      case "绊线钩":
+        i.src="imgs/tripwirehook.png";
+        break;
+      case "激活铁轨":
+        i.src="imgs/activatorrail.png";
+        break;
+      case "阳光传感器":
+        i.src="imgs/daylight.png";
+        break;
+      case "陷阱箱":
+        i.src="imgs/trapped.png";
+        break;
+      case "酿造台":
+        i.src="imgs/brewing.png";
+        break;
+      case "闪烁的西瓜":
+        i.src="imgs/glistering.png";
+        break;
+      case "发酵蜘蛛眼":
+        i.src="imgs/fermented.png";
+        break;
+      case "蛋糕":
+        i.src="imgs/cake.png";
+        break;
+      case "蘑菇煲":
+        i.src="imgs/mushroom.png";
+        break;
+      case "南瓜派":
+        i.src="imgs/pumpkinpie.png";
+        break;
+      case "兔肉煲":
+        i.src="imgs/rabbit.png";
+        break;
+      case "金萝卜":
+        i.src="imgs/goldencarrot.png";
+        break;
+      case "书":
+        i.src="imgs/book.png";
+        break;
+      case "书与笔":
+        i.src="imgs/bookandpen.png";
+        break;
+      case "地狱砖栅栏":
+        i.src="imgs/nether.png";
+        break;
+      case "附魔台":
+        i.src="imgs/ench.png";
+        break;
+      case "火焰弹":
+        i.src="imgs/firecharge.png";
+        break;
+      case "信标":
+        i.src="imgs/beacon.png";
+        break;
+      case "物品展示框":
+        i.src="imgs/item.png";
+        break;
+      case "烟花火箭":
+        i.src="imgs/firework.gif";
+        break;
+      case "烟火之星":
+        i.src="imgs/star.gif";
+        break;
+      case "拴绳":
+        i.src="imgs/lead.png";
+        break;
+      case "皮革":
+        i.src="imgs/leather.png";
+        break;
+      case "旗帜":
+        i.src="imgs/banner.png";
+        break;
+      case "盔甲架":
+        i.src="imgs/armor.png";
+        break;
+      case "青色燃料":
+        i.src="imgs/cyan.png";
+        break;
+      case "粉红色染料":
+        i.src="imgs/pink.gif";
+        break;
+      case "紫色染料":
+        i.src="imgs/purple.png";
+        break;
+      case "黄绿色染料":
+        i.src="imgs/lime.png";
+        break;
+      case "品红色染料":
+        i.src="imgs/magenta.gif";
+        break;
+      case "淡蓝色染料":
+        i.src="imgs/lightblue.gif";
+        break;
+      case "橙色染料":
+        i.src="imgs/orange.gif";
+        break;
+      case "淡灰色染料":
+        i.src="imgs/lightgray.gif";
+        break;
+      case "灰色染料":
+        i.src="imgs/gray.png";
+        break;
+      case "唱片机":
+        i.src="imgs/juke.png";
+        break;
+      case "侦测器":
+        i.src="imgs/observer.png";
+        break;
+      default:
+        alert("未找到此物品，请检查输入");
+    }
+}
